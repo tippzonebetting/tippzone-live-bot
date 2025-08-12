df53394fdd288f7215c9f520f79040243172b2ca
tippzonebetting <akospoor@gmail.com>
2025-08-13 00:09:14 +0200

    Update start_live_monitoring.py

--- start_live_monitoring.py
+++ start_live_monitoring.py
@@ -2,5 +2,5 @@ import asyncio
 from live_monitoring_bot import LiveMonitoringBot
 
 if __name__ == "__main__":
-bot = LiveMonitoringBot()
-asyncio.run(bot.start())
+    bot = LiveMonitoringBot()
+    asyncio.run(bot.start())
